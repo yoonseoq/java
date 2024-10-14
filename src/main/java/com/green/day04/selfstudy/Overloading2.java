f89826a1cedf3f35706a09ef319cb74e5bd9d9ae
yoonseoq <sysooo18@naver.com>
2024-10-14 09:32:17 +0900

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/green/day04/selfstudy/Overloading2.java
+++ src/main/java/com/green/day04/selfstudy/Overloading2.java
@@ -2,14 +2,14 @@ package com.green.day04.selfstudy;
 
 public class Overloading2 {
     public static void main(String[] args) {
-        mymathod(1,2.2);
-        mymathod(6.2,6);
+        myMathod(6,6.2);
+        myMathod(6.2,6);
     }
 //서로다른 타입
-    public static void mymathod(int a, double b){
+    public static void myMathod(int a, double b){
         System.out.println("int a, double b");
     }
-    public static void mymathod(double a, int b){
+    public static void myMathod(double a, int b){
         System.out.println("double a, int b");
     }
 
--- src/main/java/com/green/day04/selfstudy/Overloading3.java
+++ src/main/java/com/green/day04/selfstudy/Overloading3.java
@@ -5,7 +5,19 @@ public class Overloading3 {
         System.out.println(add(5,4));//int넣어도 자동으로 형변환됨
         System.out.println(add(5,2.2));
     }
-//서로다른 타입
+/*
+오버로딩이란 이름이 같고 매개변수가 다른 메서드를 여러개 정의하는 것을 메서드 오버로딩이라고 한다.
+add(int)
+add(double)
+
+반환타입이 다를경우 컴파일 오류발생
+int add(int )
+double add(int )
+
+int add (int a int b)
+int add (int c int d) 안되는 유형들
+
+ */
 
     public static double add(double a, double b){
         System.out.println("2번");
