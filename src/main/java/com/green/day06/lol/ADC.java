4dc8d4daf9d907e6638215e626ed5390482dda8f
yoonseoq <sysooo18@naver.com>
2024-10-15 19:28:19 +0900

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/green/day06/lol/ADC.java
+++ src/main/java/com/green/day06/lol/ADC.java
@@ -12,16 +12,11 @@ public class ADC {
 
         yumi(heal);//6.힐러님 저 힐좀요
         System.out.println("서폿이 힐해준 후 피통:"+heal.hp);
-
-
     }
 
     public static void yumi(int hp){
 
-
         hp=100; //2.알고보니까 다른판을 하고있는 힐러임. 얘가 체력 100을 회복시켜도 내 피통이 채워지지 않음
-
-
 
     }
     public static void yumi(Supporter heal){
