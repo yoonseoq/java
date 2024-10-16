aa6d1f0355866116514e7671d80ce7985b3c2df4
yoonseoq <sysooo18@naver.com>
2024-10-16 18:51:15 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/greenonline/assign/초급_성적관리_프로그램.java
@@ -0,0 +1,15 @@
+package com.greenonline.assign;
+
+public class 초급_성적관리_프로그램 {
+    public static void main(String[] args) {
+        Grade kbh = new Grade("강백호",90.0,85.5,70.0);
+        Grade ccs = new Grade("채치수",82.0,92.0,60.5);
+
+    }
+}
+class Grade{
+    public Grade(String name,double kor,double math, double eng){
+        System.out.printf("%s의 평균 점수:%.2f\n",name,((kor+math+eng)/3));
+    }
+
+}
---
+++ src/main/java/com/greenonline/assign/초급_합계와_평균_계산_프로그램_짜기.java
@@ -0,0 +1,50 @@
+package com.greenonline.assign;
+
+import java.util.Scanner;
+
+public class 초급_합계와_평균_계산_프로그램_짜기 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+
+        while(true){
+        System.out.println("정수를 입력하세요.");
+        int integer=scan.nextInt();
+
+        if(integer==0){
+            break;
+        }
+
+        sum += integer;
+        count++;
+
+        }
+
+        if(count>0){
+            /*
+            카운트가 되어야 평균을 구할 수 있으므로 만약 이 if문을 사용하지 않는다면
+
+             */
+            double average = (double) sum/count;
+            System.out.printf("합계는 %d입니다. 평균은 %.1f입니다.",sum,average);
+        }
+
+        //카운트가 되어야 평균을 구할 수 있으므로
+        double average = (double) sum/count;
+        System.out.printf("합계는 %d입니다. 평균은 %.1f입니다.",sum,average);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
